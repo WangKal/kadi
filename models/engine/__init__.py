fee4c966221eaa68dfbc618d08e7b3a8c758f9ba
WangKal <edgarsam.otieno@gmail.com>
2024-09-05 11:51:17 +0000

    m

---
+++ models/engine/__init__.py
@@ -0,0 +1,2 @@
+from .db import db_session,Base,init_db
+
---
+++ models/engine/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from config import Config
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    import models  # Import all models here to ensure they are registered properly on the metadata
+    Base.metadata.create_all(bind=engine)
+
