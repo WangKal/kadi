b303e854454f1078282559cdc712642ab4854590
WangKal <30478977+WangKal@users.noreply.github.com>
2024-09-11 23:28:12 +0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -96,7 +96,7 @@ def register_challenge():
                 # Handle JSON decoding errors
                 return jsonify({
                     'status': False,
-                    'message':  user_id
+                    'message':  response.text
                 }), 500
 
             # Check if the external API call was successful
